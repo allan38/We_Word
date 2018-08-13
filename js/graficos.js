7f286cfd3c7ea07920f1acf29ccf6d97ef38ca9f
Your Name <you@example.com>
2018-08-13 09:14:51 -0300

    arquivos de tela WW

---
+++ js/graficos.js
@@ -0,0 +1,223 @@
+
+		
+			AmCharts.makeChart("graf1",
+				{
+					"type": "pie",
+					"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+					"colors": [
+						"#FF0F00",
+						"#009688",
+						"#FF9E01",
+						"#FCD202",
+						"#F8FF01",
+						"#B0DE09",
+						"#04D215",
+						"#0D8ECF",
+						"#0D52D1",
+						"#2A0CD0",
+						"#8A0CCF",
+						"#CD0D74",
+						"#754DEB",
+						"#DDDDDD",
+						"#999999",
+						"#333333",
+						"#000000",
+						"#57032A",
+						"#CA9726",
+						"#990000",
+						"#4B0C25"
+					],
+					"titleField": "category",
+					"valueField": "column-1",
+					"allLabels": [],
+					"balloon": {},
+					"legend": {
+						"enabled": true,
+						"align": "center",
+						"markerType": "circle"
+					},
+					"titles": [],
+					"dataProvider": [
+						{
+							"category": "Atrasado",
+							"column-1": 8
+						},
+						{
+							"category": "Realizado",
+							"column-1": 6
+						},
+						{
+							"category": "Andamento",
+							"column-1": 2
+						}
+					]
+				}
+			);
+	
+
+			AmCharts.makeChart("graf2",
+				{
+					"type": "pie",
+					"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+					"colors": [
+						"#FF0F00",
+						"#009688",
+						"#FF9E01",
+						"#FCD202",
+						"#F8FF01",
+						"#B0DE09",
+						"#04D215",
+						"#0D8ECF",
+						"#0D52D1",
+						"#2A0CD0",
+						"#8A0CCF",
+						"#CD0D74",
+						"#754DEB",
+						"#DDDDDD",
+						"#999999",
+						"#333333",
+						"#000000",
+						"#57032A",
+						"#CA9726",
+						"#990000",
+						"#4B0C25"
+					],
+					"titleField": "category",
+					"valueField": "column-1",
+					"allLabels": [],
+					"balloon": {},
+					"legend": {
+						"enabled": true,
+						"align": "center",
+						"markerType": "circle"
+					},
+					"titles": [],
+					"dataProvider": [
+						{
+							"category": "Atrasado",
+							"column-1": "0"
+						},
+						{
+							"category": "Realizado",
+							"column-1": 6
+						},
+						{
+							"category": "Andamento",
+							"column-1": 2
+						}
+					]
+				}
+				);
+
+
+				AmCharts.makeChart("graf3",
+				{
+					"type": "pie",
+					"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+					"colors": [
+						"#FF0F00",
+						"#009688",
+						"#FF9E01",
+						"#FCD202",
+						"#F8FF01",
+						"#B0DE09",
+						"#04D215",
+						"#0D8ECF",
+						"#0D52D1",
+						"#2A0CD0",
+						"#8A0CCF",
+						"#CD0D74",
+						"#754DEB",
+						"#DDDDDD",
+						"#999999",
+						"#333333",
+						"#000000",
+						"#57032A",
+						"#CA9726",
+						"#990000",
+						"#4B0C25"
+					],
+					"titleField": "category",
+					"valueField": "column-1",
+					"allLabels": [],
+					"balloon": {},
+					"legend": {
+						"enabled": true,
+						"align": "center",
+						"markerType": "circle"
+					},
+					"titles": [],
+					"dataProvider": [
+						{
+							"category": "Atrasado",
+							"column-1": "1"
+						},
+						{
+							"category": "Realizado",
+							"column-1": "3"
+						},
+						{
+							"category": "Andamento",
+							"column-1": "6"
+						}
+					]
+				}
+
+				);
+
+
+
+				AmCharts.makeChart("atvs",
+				{
+					"type": "pie",
+					"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+					"colors": [
+						"#4caf50",
+						"#f44336",
+						"#ff9800",
+						"#FCD202",
+						"#F8FF01",
+						"#B0DE09",
+						"#04D215",
+						"#0D8ECF",
+						"#0D52D1",
+						"#2A0CD0",
+						"#8A0CCF",
+						"#CD0D74",
+						"#754DEB",
+						"#DDDDDD",
+						"#999999",
+						"#333333",
+						"#000000",
+						"#57032A",
+						"#CA9726",
+						"#990000",
+						"#4B0C25"
+					],
+					"gradientRatio": [],
+					"titleField": "category",
+					"valueField": "column-1",
+					"allLabels": [],
+					"balloon": {},
+					"legend": {
+						"enabled": true,
+						"align": "center",
+						"markerType": "circle"
+					},
+					"titles": [],
+					"dataProvider": [
+						{
+							"category": "Concluidas",
+							"column-1": "32"
+						},
+						{
+							"category": "Atrasadas",
+							"column-1": "12"
+						},
+						{
+							"category": "Pendentes",
+							"column-1": "20"
+						}
+					]
+				}
+			);
